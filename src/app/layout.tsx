43c78f9dfb71eebc8887492762b2c3886e69218b
Greenwood <greenwood672.dev@gmail.com>
2025-04-19 16:12:08 +0100

    Added analystics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { NavBar } from "@/components/nav";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export const metadata: Metadata = {
   title: "Oliver Greenwood",
   description: "Yours Truly",
@@ -18,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`antialiased`}>
+        <Analytics />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="min-h-screen px-8 py-1 overflow-hidden relative">
             <NavBar />
